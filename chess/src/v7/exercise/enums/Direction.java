a8a1e3ad3fd5df512609e5efd2dabe388024fe52
ddominguezh <diegoalbert500@gmail.com>
2020-11-09 12:34:55 +0100

    Update and rename Position.java to Direction.java

--- chess/src/v7/exercise/enums/Direction.java
+++ chess/src/v7/exercise/enums/Direction.java
@@ -1,6 +1,6 @@
 package v7.exercise.enums;
 
-public enum Position {
+public enum Direction {
 	VERTICAL_FORWARD,
 	VERTICAL_BACK,
 	HORIZONTAL_RIGHT,
@@ -11,43 +11,43 @@ public enum Position {
 	DIAGONAL_BACK_RIGHT,
 	L_SHAPE;
 
-	public static Position valueOf(int compareX, int compareY) {
+	public static Direction valueOf(int compareX, int compareY) {
 		if(compareY == 0 && compareX < 0) {
-			return Position.VERTICAL_FORWARD;
+			return Direction.VERTICAL_FORWARD;
 		}
 		if(compareY == 0 && compareX > 0) {
-			return Position.VERTICAL_BACK;
+			return Direction.VERTICAL_BACK;
 		}
 		if(compareX == 0 && compareY < 0) {
-			return Position.HORIZONTAL_RIGHT;
+			return Direction.HORIZONTAL_RIGHT;
 		}
 		if(compareX == 0 && compareY > 0) {
-			return Position.HORIZONTAL_LEFT;
+			return Direction.HORIZONTAL_LEFT;
 		}
 		if(Math.abs(compareX) == Math.abs(compareY) && compareX < 0 && compareY < 0) {
-			return Position.DIAGONAL_FORWARD_LEFT;
+			return Direction.DIAGONAL_FORWARD_LEFT;
 		}
 		if(Math.abs(compareX) == Math.abs(compareY) && compareX < 0 && compareY > 0) {
-			return Position.DIAGONAL_FORWARD_RIGHT;
+			return Direction.DIAGONAL_FORWARD_RIGHT;
 		}
 		if(Math.abs(compareX) == Math.abs(compareY) && compareX > 0 && compareY < 0) {
-			return Position.DIAGONAL_BACK_LEFT;
+			return Direction.DIAGONAL_BACK_LEFT;
 		}
 		if(Math.abs(compareX) == Math.abs(compareY) && compareX > 0 && compareY > 0) {
-			return Position.DIAGONAL_BACK_RIGHT;
+			return Direction.DIAGONAL_BACK_RIGHT;
 		}
 		if(Math.abs(compareX) + Math.abs(compareY) == 3) {
-			return Position.L_SHAPE;
+			return Direction.L_SHAPE;
 		}
 		return null;
 	}
 
 	public boolean isVertical() {
-		return Position.VERTICAL_FORWARD.equals(this) || Position.VERTICAL_BACK.equals(this);
+		return Direction.VERTICAL_FORWARD.equals(this) || Direction.VERTICAL_BACK.equals(this);
 	}
 
 	public boolean isHorizontal() {
-		return Position.HORIZONTAL_LEFT.equals(this) || Position.HORIZONTAL_RIGHT.equals(this);
+		return Direction.HORIZONTAL_LEFT.equals(this) || Direction.HORIZONTAL_RIGHT.equals(this);
 	}
 	
 	public boolean isDiagonal() {
@@ -55,15 +55,15 @@ public enum Position {
 	}
 
 	public boolean isDiagonalForward() {
-		return Position.DIAGONAL_FORWARD_LEFT.equals(this) || Position.DIAGONAL_FORWARD_RIGHT.equals(this);
+		return Direction.DIAGONAL_FORWARD_LEFT.equals(this) || Direction.DIAGONAL_FORWARD_RIGHT.equals(this);
 	}
 	
 	public boolean isDiagonalBack() {
-		return Position.DIAGONAL_BACK_LEFT.equals(this) || Position.DIAGONAL_BACK_RIGHT.equals(this);
+		return Direction.DIAGONAL_BACK_LEFT.equals(this) || Direction.DIAGONAL_BACK_RIGHT.equals(this);
 	}
 	
 	public boolean isMinor() {
-		return Position.VERTICAL_FORWARD.equals(this) || Position.HORIZONTAL_RIGHT.equals(this) || Position.DIAGONAL_FORWARD_LEFT.equals(this) || Position.DIAGONAL_FORWARD_RIGHT.equals(this);
+		return Direction.VERTICAL_FORWARD.equals(this) || Direction.HORIZONTAL_RIGHT.equals(this) || Direction.DIAGONAL_FORWARD_LEFT.equals(this) || Direction.DIAGONAL_FORWARD_RIGHT.equals(this);
 	}
 	
 }
