817b8ede8793bdce0db4b596d6176cbbef028323
ddominguezh <diegoalbert500@gmail.com>
2020-11-09 22:18:18 +0100

    Update Player.java

--- chess/src/v7/exercise/Player.java
+++ chess/src/v7/exercise/Player.java
@@ -15,6 +15,16 @@ public class Player {
 
 	private Color color;
 	private List<String> movements;
+	private DataPiece[] pieces = new DataPiece[] {
+			DataPiece.ROOK,
+			DataPiece.KNIGHT,
+			DataPiece.BISHOP,
+			DataPiece.QUEEN,
+			DataPiece.KING,
+			DataPiece.BISHOP,
+			DataPiece.KNIGHT,
+			DataPiece.ROOK
+	};
 	
 	public Player(Color color, Coordinate[] lastRow, Coordinate[] pawnRow) {
 		this.color = color;
@@ -22,14 +32,13 @@ public class Player {
 		for( int i = 0 ; i < pawnRow.length ; i++ ) {
 			pawnRow[i].add(new Pawn(this));
 		}
-		lastRow[0].add(new Rook(this));
-		lastRow[1].add(new Knight(this));
-		lastRow[2].add(new Bishop(this));
-		lastRow[3].add(new Queen(this));
-		lastRow[4].add(new King(this));
-		lastRow[5].add(new Bishop(this));
-		lastRow[6].add(new Knight(this));
-		lastRow[7].add(new Rook(this));
+		try {
+			for( int i = 0 ; i < pieces.length ; i++ ) {
+				lastRow[i].add(pieces[i].newInstance(this));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public boolean isWhite() {
