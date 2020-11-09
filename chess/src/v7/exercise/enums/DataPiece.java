bc512e40091a82f7b1eea0d1cc24aa6abdff52a2
ddominguezh <diegoalbert500@gmail.com>
2020-11-09 22:16:51 +0100

    Update DataPiece.java

--- chess/src/v7/exercise/enums/DataPiece.java
+++ chess/src/v7/exercise/enums/DataPiece.java
@@ -1,18 +1,25 @@
 package v7.exercise.enums;
 
 public enum DataPiece {
-	PAWN('\u2659', '\u265F'),
-	ROOK('\u2656', '\u265C'),
-	KNIGHT('\u2658', '\u265E'),
-	BISHOP('\u2657', '\u265D'),
-	QUEEN('\u2655', '\u265B'),
-	KING('\u2654', '\u265A');
+	PAWN("Pawn",'\u2659', '\u265F'),
+	ROOK("Rook",'\u2656', '\u265C'),
+	KNIGHT("Knight",'\u2658', '\u265E'),
+	BISHOP("Bishop",'\u2657', '\u265D'),
+	QUEEN("Queen",'\u2655', '\u265B'),
+	KING("King",'\u2654', '\u265A');
+	private String classname;
 	private char white;
 	private char black;
 	
-	DataPiece(char white, char black){
+	DataPiece(String classname, char white, char black){
+		this.classname = "v7.exercise.pieces." + classname;
 		this.white = white;
 		this.black = black;
+	}
+	
+	@SuppressWarnings("static-access")
+	public Piece newInstance(Player player) throws Exception {
+		return (Piece) Piece.class.forName(this.classname).getDeclaredConstructor(player.getClass()).newInstance(player);
 	}
 	
 	public char getName(boolean isWhite) {
