6b3fc0f3ccceed360dd6a0df7ce2ba76352027a0
ddominguezh <diegoalbert500@gmail.com>
2020-11-10 17:58:00 +0100

    Update Pawn.java

--- chess/src/v7/exercise/pieces/Pawn.java
+++ chess/src/v7/exercise/pieces/Pawn.java
@@ -7,22 +7,21 @@ import v7.exercise.enums.Position;
 
 public class Pawn extends Piece{
 
-	//PEON
-	public Pawn(Player player) {
-		super(player, DataPiece.PAWN);
+	public Pawn(Color color) {
+		super(color, DataPiece.PAWN);
 	}
 
 	@Override
 	public boolean canMove(Coordinate origin, Coordinate destination) {
 		Direction direction = origin.getDirection(destination);
 		int distance = origin.distance(destination);
-		if(destination.containsPieceOfOpponent(this.player)) {
-			return distance == 1 && ((this.player.isWhite() && direction.isDiagonalForward()) 
-					|| (!this.player.isWhite() && direction.isDiagonalBack()));
+		if(destination.containsPieceOfOpponent(this.color)) {
+			return distance == 1 && ((Color.WHITE.equals(this.color) && direction.isDiagonalForward()) 
+					|| (Color.BLACK.equals(this.color) && direction.isDiagonalBack()));
 		}
 		if(distance == 1 || (distance == 2 && this.initialPosition)) {
-			return ((this.player.isWhite() && Direction.VERTICAL_FORWARD.equals(direction)) || 
-					(!this.player.isWhite() && Direction.VERTICAL_BACK.equals(direction)));
+			return ((Color.WHITE.equals(this.color) && Direction.VERTICAL_FORWARD.equals(direction)) || 
+					(Color.BLACK.equals(this.color) && Direction.VERTICAL_BACK.equals(direction)));
 		}
 		return false;
 	}
